f9359e17d9b4650a3626f9b44cac7b0ea35c9211
Adept <adeptiumion@gmail.com>
2023-12-13 16:37:27 +0700

    commitByAdept

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,11 +2,20 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
+
+
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld secondBean = applicationContext.getBean("helloworld", HelloWorld.class);
+        Cat firstCat = applicationContext.getBean("cat", Cat.class);
+        Cat secondCat = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(bean == secondBean);
+        System.out.println(firstCat == secondCat);
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,13 +2,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.util.logging.Logger;
+
 @Configuration
 public class AppConfig {
- 
-    @Bean(name="helloworld")
+
+    public static Logger configLogger = Logger.getLogger(AppConfig.class.getName());
+
+    @Bean(name = "helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
+        configLogger.info("HelloWorld bean has been creating!");
         return helloWorld;
+    }
+
+    @Bean("cat")
+    @Scope("prototype") // 4. Настройте этот бин так, чтобы он создавался новым при каждом запросе.
+    public Cat getCat() {
+        configLogger.info("Cat bean has been creating!");
+        return new Cat();
     }
 }
